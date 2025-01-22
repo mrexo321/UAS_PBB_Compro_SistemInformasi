d43052921f715b27484f5526788475eb5def3035
wa1t <azkanyan@gmail.com>
2025-01-22 13:38:43 +0700

    Splash Screen Added

---
+++ app/src/main/java/com/example/uas_ppb_compro_sisteminformasi/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.uas_ppb_compro_sisteminformasi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public int time = 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+//        Trigger intent splash by time
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(SplashActivity.this, MainActivity.class);
+                startActivities(new Intent[]{home});
+                finish();
+            }
+        }, time);
+    }
+}
